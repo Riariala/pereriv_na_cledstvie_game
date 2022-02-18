76d9f6142eef3ecc617b8e6a6846c20b0e0824d8
Riala <Sandra.lapokha@gmail.com>
2022-02-18 16:03:14 +1000

    Auto stash before rebase of "origin/chris"

--- Assets/scripts/Objects/ObjectManager.cs
+++ Assets/scripts/Objects/ObjectManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class ObjectManager : MonoBehaviour
 {
       //порядок: осмотреть, взять, взаимодействовать;       соответствует порядку расположения кнопок в префабе menu_obj_touch 
-    [SerializeField]  public bool[] enable_actions = new bool[] {false, false, false};  
+      
+      [SerializeField]  public bool[] enable_actions = new bool[] {false, false, false};  
     
        // // Update is called once per frame
     // void Update()
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -13,38 +13,27 @@ using Photon.Bolt.Utils;
 
 public class PlayerController : Photon.Bolt.EntityBehaviour<ICustomPlayer>//MonoBehaviour
 {
-    private Rigidbody _rb;
+    public Rigidbody _rb;
     //private Transform _transform;
     [SerializeField] private FixedJoystick _joystick;
+    private Collider _collider;
+    private Vector3 closest_point;
+    private Vector3 change_pos;
     //[SerializeField] private Animator _animator;
 
     //[SerializeField] private float speed =  10f;
     private float speed = 170f;
 
-    /*void Start()
-    {
-        //_rb = GetComponent<Rigidbody>();
-        //state.SetTransforms(state.PlayerTransform, transform);
-    }*/
+    
     public override void Attached()
     {
         _rb = transform.GetChild(0).GetComponent<Rigidbody>();
         //_rb = GetComponent<Rigidbody>();
         state.SetTransforms(state.PlayerTransform, transform);
+        
         //ChangeJoystick(_joystick);
         //_transform = transform;
     }
-
-    // void Update()
-    // {
-
-    // }
-
-    /*public override void Attached()
-    {
-        _rb = GetComponent<Rigidbody>();
-        state.SetTransforms(state.PlayerTransform, transform);
-    }*/
 
     public override void SimulateOwner()
     {
@@ -55,8 +44,38 @@ public class PlayerController : Photon.Bolt.EntityBehaviour<ICustomPlayer>//Mono
             _rb.velocity = new Vector3(_joystick.Horizontal * speed * BoltNetwork.FrameDeltaTime, _rb.velocity.y, _joystick.Vertical * speed* BoltNetwork.FrameDeltaTime);
         }
     }
-        
 
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log((other.transform.position-transform.position).normalized);
+            //transform.position = transform.position + (other.transform.position-transform.position).normalized * -0.1f;
+            //transform.position = other.transform.position - (other.transform.position - transform.position);
+            Debug.Log(other.bounds.size);
+            Debug.Log(this.GetComponent<CapsuleCollider>());
+            Vector3 normalised_distance = (other.transform.position-transform.position).normalized * -1;
+            Vector3 other_size = other.bounds.size;
+            Vector3 distance_teleport = new Vector3(normalised_distance.x * other_size.x, transform.position.y, normalised_distance.z * other_size.z);
+            //transform.position = distance_teleport;
+            this.GetComponent<Rigidbody>().AddForce (normalised_distance  * 10F);
+            //transform.position = transform.position + transform.GetComponent<Rigidbody>().velocity * (- BoltNetwork.FrameDeltaTime * speed);
+            //this.GetComponent<Rigidbody>().velocyty = 
+            //this.gameObject.GetComponent<PlayerController>().GoBack();
+            //_rb.velocity = new Vector3(0,0,0);
+            //Debug.Log(this.GetComponent<CapsuleCollider>().gameObject.name);
+            // Transform tr = this.GetComponent<CapsuleCollider>().gameObject.transform;
+            // tr.position = new Vector3( -1 * _joystick.Horizontal * speed * BoltNetwork.FrameDeltaTime + tr.position.x, tr.position.y, -1 * _joystick.Vertical * speed * BoltNetwork.FrameDeltaTime + tr.position.z);
+        }
+    }
+
+    public void GoBack()
+    {
+        Debug.Log(_joystick);
+        //_rb.velocity = new Vector3(0,0,0);
+    }
+        
     public void ChangeJoystick(FixedJoystick newJstk)
     {
         _joystick = newJstk;
