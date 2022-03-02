d9fc5167dc4c4571bc4d601c3be3c8a56cedb356
Riala <Sandra.lapokha@gmail.com>
2022-03-02 23:55:31 +1000

    Появление меню при скроллинге

--- Assets/scripts/Objects/touchMenu.cs
+++ Assets/scripts/Objects/touchMenu.cs
@@ -3,82 +3,88 @@ using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class touchMenu : MonoBehaviour
 {
 
     [SerializeField] public GameObject menu_obj_touch;
     [SerializeField] public Transform parent;
+    [SerializeField] public GameObject inventory_panel;
 
     private GameObject menu;
     private GameObject touched_item;
+    private Vector2 start_pos;
+    private Vector2 direction;
+    private float screen_height;
+
+    void Start()
+    {
+        screen_height = Screen.height;
+    }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.touchCount == 1)
         {
 
+            Touch touch = Input.GetTouch(0);
 
-
-
-
-        }
-
-
-
-
-
-
-
-            //обработка нажатия мышки, тестовое для работы на ноуте, для обработки нажатия пальцем раскомментить строки и закомментить ону строку под каждой
-            //if (Input.touchCount == 1)                   //НЕ УДАЛЯТЬ
-            if (Input.GetMouseButtonDown(0))
-        {
-            Debug.Log("hit");
-            //Touch touch = Input.GetTouch(0);        //НЕ УДАЛЯТЬ
-
-
-            //if ((menu != null ) && (!EventSystem.current.IsPointerOverGameObject(touch.fingerId)))
-            if ((menu != null ) && (!EventSystem.current.IsPointerOverGameObject()))
+            switch (touch.phase)
             {
-                Destroy(menu);
-            }
+                case TouchPhase.Began:
+                    start_pos = touch.position;
+                    break;
 
-          //Vector3 pos = touch.position;           //НЕ УДАЛЯТЬ
-            Vector3 pos = Input.mousePosition;
-            Ray ray = Camera.main.ScreenPointToRay(pos);
-            //Ray ray = Camera.ScreenPointToRay(pos);
-            RaycastHit hit;
-            
+                case TouchPhase.Moved:
+                    break;
 
-            if ((menu == null) && (Physics.Raycast(ray, out hit)))
-            {
-                
-                Debug.Log(hit.collider.tag);
-                
-                if (hit.collider.tag == "Item")
-                {   
-                    touched_item = hit.collider.gameObject;
-                    menu = Instantiate(menu_obj_touch, parent);
-                    menu.transform.position = pos;
-                    List<bool> enable_actionsl = new List<bool>(touched_item.GetComponent<ObjectManager>().enable_actions);
-                    int count_actions = enable_actionsl.Count(x => x == true);
-
-                    float koef =  Mathf.PI * 2 / count_actions;
-                    int indActiveAction = 0;
-                    for (int i = 0; i < enable_actionsl.Count; i++)
+                case TouchPhase.Ended:
+                    direction = touch.position - start_pos;
+                    if ((direction.y <= -50) && (start_pos.y <= 75))
                     {
-                        if (enable_actionsl[i])
+                        inventory_panel.SetActive(true);
+                    }
+
+                    if ((direction.y >= 50) && (start_pos.y >= screen_height - 75))
+                    {
+                        inventory_panel.SetActive(false);
+                    }
+
+                    if (menu != null){Destroy(menu);}
+
+                    Vector3 pos = touch.position;
+                    Ray ray = Camera.main.ScreenPointToRay(pos);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+
+                        if (hit.collider.tag == "Item")
                         {
-                            GameObject btn = menu.transform.GetChild(i).gameObject;
-                            btn.SetActive(true); 
-                            btn.transform.localPosition  = new Vector3(Mathf.Cos(indActiveAction * koef)*60, Mathf.Sin(indActiveAction * koef)*60, 0);
-                            indActiveAction++;
+                            touched_item = hit.collider.gameObject;
+                            menu = Instantiate(menu_obj_touch, parent);
+                            menu.transform.position = pos;
+                            List<bool> enable_actionsl = new List<bool>(touched_item.GetComponent<ObjectManager>().enable_actions);
+                            int count_actions = enable_actionsl.Count(x => x == true);
+
+                            float koef = Mathf.PI * 2 / count_actions;
+                            int indActiveAction = 0;
+                            for (int i = 0; i < enable_actionsl.Count; i++)
+                            {
+                                if (enable_actionsl[i])
+                                {
+                                    GameObject btn = menu.transform.GetChild(i).gameObject;
+                                    btn.SetActive(true);
+                                    btn.transform.localPosition = new Vector3(Mathf.Cos(indActiveAction * koef + (Mathf.PI / 2)) * 60, Mathf.Sin(indActiveAction * koef + (Mathf.PI / 2)) * 60, 0);
+                                    indActiveAction++;
+                                }
+                            }
                         }
                     }
-                }
+
+                    break;
             }
-         }
+        }
     }
 
     //кнопки менюшки объектов, позже приделаю сюда нормальную реализацию, а не просто подписи
