2911684c87a25f0110a4f3548497030374f2d449
Riala <Sandra.lapokha@gmail.com>
2022-05-25 04:49:05 +1000

    Обработка эффектов, варианты ответов в диалогах, заблокированные варианты ответов

---
+++ Assets/scripts/Objectclasses/DialogVariant.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogVariant
+{
+    public int variantId;
+    public List<string> optionLines;
+    public List<ObjectActions> changes;
+    public List<int> effects;
+    public List<bool> available;
+    public List<List<int>> specailActions;
+}
---
+++ Assets/scripts/Objectclasses/OpenedDialogs.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenedDialogs
+{
+    public int dialogId;
+    public int variantId;
+    public bool newMeaning;
+}
