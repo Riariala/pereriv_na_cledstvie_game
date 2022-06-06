68a80bef0ec33062ed4eb13a1774e6c697e8be6c
Кристина Лутошкина <krislyt@mail.ru>
2022-06-06 18:49:38 +1000

    Опредление персонажа у игрока

---
+++ Assets/scripts/Network/CharToken.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UdpKit;
+using UnityEngine.SceneManagement;
+using UdpKit.Platform.Photon;
+using Photon.Bolt;
+using Photon.Bolt.Matchmaking;
+using Photon.Bolt.Utils;
+
+public class CharToken : Photon.Bolt.PooledProtocolToken
+{
+    public bool isPlayer1;
+    public override void Read(UdpPacket packet)
+    {
+        // Deserialize Token data
+        isPlayer1 = packet.ReadBool();
+    }
+
+    public override void Write(UdpPacket packet)
+    {
+        // Serialize Token Data
+        packet.WriteBool(isPlayer1);
+    }
+
+    public override void Reset()
+    {
+        // Reset Token Data
+        isPlayer1 = default(bool);
+    }
+}
--- Assets/scripts/Network/NetworkCallbacks.cs
+++ Assets/scripts/Network/NetworkCallbacks.cs
@@ -2,16 +2,38 @@ using UnityEngine;
 using UdpKit.Platform.Photon;
 using Photon.Bolt;
 
-
+[BoltGlobalBehaviour]
 public class NetworkCallbacks : GlobalEventListener
 {
     public GameObject player1;
     public GameObject player2;
+    public bool isPlayer1=true;
+    public CharReader reader;
+
+    public override void BoltStartBegin()
+    {
+        BoltNetwork.RegisterTokenClass<CharToken>();
+
+    }
 
     public override void SceneLoadLocalDone(string scene, IProtocolToken token)
     {
+        if (BoltNetwork.IsClient)
+        {
+            isPlayer1 = !reader.isPlayer1;
+        }
         var spawnPos = new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5));
-        BoltNetwork.Instantiate(player1, spawnPos, Quaternion.identity);
+        if (isPlayer1)
+        {
+            BoltNetwork.Instantiate(player1, spawnPos, Quaternion.identity);
+            Debug.Log("1");
+        }
+        else
+        {
+            BoltNetwork.Instantiate(player2, spawnPos, Quaternion.identity);
+            Debug.Log("2");
+        }
+        
     }
 
     /*public override void SceneLoadLocalDone(string map)
